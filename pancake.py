adef18e0c0e132c48fdf16735c522ee3af850e71
abtheo <c.theo.kent@gmail.com>
2021-04-22 15:51:57 +0200

    Setting token address and value

--- pancake.py
+++ pancake.py
@@ -3,6 +3,11 @@ from time import sleep
 from tkinter import Tk
 import numpy as np
 
+#Initialise some globals
+bnb_address = "0xB8c77482e45F1F44dE1745F52C74426C631bDD52"
+FROM_BOX = True
+TO_BOX = False
+
 def copy_to_clipboard(all=True):
     """Copy the current selection to the clipboard
     
@@ -15,6 +20,14 @@ def copy_to_clipboard(all=True):
     gui.press("c")
     gui.keyUp("ctrl")
 
+def select_box(from_box):
+    if from_box:
+        #From box / balance
+        gui.click(x=922, y=454)
+    else:
+        #To box / exchange rate
+        gui.click(x=922, y=608)
+
 def get_balance(from_box):
     """Get the current balance 
     by copying the correct box on Pancakeswap
@@ -24,39 +37,34 @@ def get_balance(from_box):
                 -True to select From balance,
                 -False to select To balance
     """
-    if from_box:
-        #From box / balance
-        gui.click(x=922, y=454)
-    else:
-        #To box / exchange rate
-        gui.click(x=922, y=608)
-
+    select_box(from_box)
     sleep(0.5)
     copy_to_clipboard()
     return np.float(Tk().clipboard_get())
 
-def set_token_value(from_box):
-    if from_box:
-        #From box / balance
-        gui.click(x=922, y=454)
-    else:
-        #To box / exchange rate
-        gui.click(x=922, y=608)
+def set_token_value(from_box, value):
+    select_box(from_box)
+    gui.write(value)
     
 def set_token_address(from_box, address):
     if from_box:
         #From box / balance
-        gui.click(x=922, y=454)
+        gui.click(x=1240, y=454)
     else:
         #To box / exchange rate
-        gui.click(x=922, y=608)
+        gui.click(x=1240, y=608)
     
-
+    sleep(0.6)
+    #Click Address box
+    gui.click(x=890, y=280)
+    #Fill contract address
+    gui.write(address)
 
 
 """START BOT:"""
 #Assume we are in browser focus on Pancakeswap Exchange tab
 if __name__ == '__main__':
     sleep(5)
-    print(get_balance(True), to_balance(False))
-
+    print(get_balance(FROM_BOX), to_balance(TO_BOX))
+    set_token_address(FROM_BOX, bnb_address)
+    set_token_value(1000, FROM_BOX)
