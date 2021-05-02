043d6b95eab71f4067521a1cf383bf51cf1cf7e5
sceaf <elixirofmimir@gmail.com>
2021-05-02 16:47:24 +0100

    paper trader

--- shitcoins.py
+++ shitcoins.py
@@ -3,17 +3,14 @@ import token_profiler.profiler as profiler
 import blockchain.trader as Trading
 import pandas as pd
 import threading
-from datetime import *
-# Step 1 - scrape Reddit
-# Step 2 - store token ID locally/deduplicate
-# Step 3 - get dictionary from bscscan.py (profiler)
+from datetime import datetime, timedelta
 # Step 4 - do some logic to figure out how much to buy, choose trading strategy
 # Step 5 - monitor price (pikey code from trader.py)
 # Step 6 - Use trader.py to buy/sell
 
 # Thread class to track and trade a specific shitcoin contract address
 class Shitcoin(threading.Thread):
-    def __init__(self, contract, profile):
+    def __init__(self, contract, profile, bnb, trader):
         threading.Thread.__init__(self)
 
         # Establish whether pancakeswap v1 or v2 is better
@@ -22,27 +19,57 @@ class Shitcoin(threading.Thread):
         else: self.type = 'v2'
 
         stats = profile['stats']
-
+        self.contract = contract
         self.sellExists = profile['sell_exists']
+        self.trader = trader
+        self.earliest_tx = stats['age'].to_pydatetime()
+        self.dateSeen = datetime.now()
+        self.bnb = bnb
 
-        self.earliest_tx = stats['age']
-        self.dateSeen = pd.Timestamp.now(tz=timezone.utc)
+    def currentPrice(self):
+        self.trader.getCurrentPrice(self.contract)
+
+    def printBalance(self):
+        print(self.contract + ": You have " + self.bnb + " BNB and " + self.shitcoin + " tokens.")
+
+    def buy(self, bnb):
+        self.bnb -= bnb
+        self.shitcoin += bnb / currentPrice()
+        print(self.contract + ": Bought " + self.shitcoin + " for " + currentPrice())
+
+
+    def sell(self, shitcoin):
+        self.shitcoin -= shitcoin
+        self.bnb += shitcoin * currentPrice()
+        print(self.contract + ": Sold " + self.shitcoin + " for " + currentPrice())
+
+    def earlyEntryStrategy(self):
+        entryPrice = currentPrice()
+        buy(min(0.05, bnb))
+        while (True):
+            price = currentPrice()
+            if (price < 0.6 * entryPrice) or (price > 3  * entryPrice):
+                break
+        sell(self.shitcoin)
+        printBalance()
 
     def run(self):
         if not self.sellExists:
             return
-        pass
+        if (self.dateSeen - self.earliest_tx) > timedelta(hours=1):
+            return
+        earlyEntryStrategy()
 
 # Class for overseeing the trading of shitcoins, and
 class Tracker:
     def __init__(self):
-        #self.trader = Trading.Trader()
+        self.trader = Trading.Trader()
         self.tokenProfiler = profiler.Profiler()
         self.tokenDict = {}
 
     def track(self):
         while(True):
-            redditTokens = reddit_scraper.scrape_subreddits(time=30)
+            redditTokens = reddit_scraper.scrape_subreddits(time=10)
             addresses = [a for a in redditTokens["address"] if a != '']
 
             for a in addresses:
@@ -51,13 +78,10 @@ class Tracker:
                     print("Address: " + a)
                     profile = self.tokenProfiler.profile_token(a)
                     print(profile['stats'])
-                    self.tokenDict[a] = Shitcoin(a, profile)
+                    self.tokenDict[a] = Shitcoin(a, profile, 0.1, self.trader)
                     self.tokenDict[a].run()
 
-            # Converting to Dictionary removes duplicates and allows contextualising
-            self.tokenDict = dict.fromkeys(addresses)
-            print(tokenDict)
-            time.wait(20)
+            time.wait(10)
 
         #print("Profiling MoonCunt:")
         #print(self.tokenProfiler.profile_token('0x5bf5a3c97dd86064a6b97432b04ddb5ffcf98331'))
