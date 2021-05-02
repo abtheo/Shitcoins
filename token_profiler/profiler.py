f173d503913da8634c598a92dd37c742d086f57c
abtheo <c.theo.kent@gmail.com>
2021-05-02 05:41:53 +0200

    Refactored token profiling into master class, profiler.py

--- token_profiler/profiler.py
+++ token_profiler/profiler.py
@@ -43,11 +43,11 @@ class Profiler:
         sleep(2)
 
         #Get links to BSCScan for Liquidity Providers
-        v1_lp_holders = self.driver.find_element_by_xpath(
+        v1_lp_address = self.driver.find_element_by_xpath(
             "//*[@id='root']/div/div[1]/div[2]/div/div[1]/div[2]/small/a[2]"
         ).get_attribute('href')
 
-        v2_lp_holders = self.driver.find_element_by_xpath(
+        v2_lp_address = self.driver.find_element_by_xpath(
             "//*[@id='root']/div/div[1]/div[2]/div/div[1]/div[2]/small/a[4]"
         ).get_attribute('href')
 
@@ -64,8 +64,8 @@ class Profiler:
 
         return {
             "sell_exists": bool(sell_txs),
-            "v1_lp_address": v1_lp_holders,
-            "v2_lp_address": v2_lp_holders,
+            "v1_lp_address": v1_lp_address,
+            "v2_lp_address": v2_lp_address,
             "v1_bnb_holdings": v1_bnb,
             "v2_bnb_holdings": v2_bnb
         }
@@ -86,12 +86,10 @@ class Profiler:
         # Extract total number of transactions
         transactions = self.driver.find_element_by_id("totaltxns").text
         num_transactions = int(re.sub("[^0-9]", "", transactions))
-        print(f"Number of transactions: {num_transactions}")
 
         # Extract number of token holders
         holders = self.driver.find_element_by_class_name("mr-3").text
         num_holders = int(re.sub("[^0-9]", "", holders))
-        print(f"Number of token holders: {num_holders}")
 
         # Focus TX table
         WebDriverWait(self.driver, 5).until(EC.frame_to_be_available_and_switch_to_it(
@@ -118,6 +116,8 @@ class Profiler:
         df["Date Time (UTC)"] = pd.to_datetime(df["Date Time (UTC)"])
         earliest_tx = df["Date Time (UTC)"].min()
 
+        #TODO: Hunt for Whales
+
         return {
             "num_transactions": num_transactions,
             "num_holders" : num_holders,
@@ -139,8 +139,7 @@ class Profiler:
 
         # Extract number of token holders
         holders = self.driver.find_element_by_class_name("mr-3").text
-        num_holders = int(re.sub("[^0-9]", "", holders))
-        print(f"Number of token holders: {num_holders}")
+        num_lp_holders = int(re.sub("[^0-9]", "", holders))
 
         # Focus holders table
         WebDriverWait(self.driver, 5).until(EC.frame_to_be_available_and_switch_to_it(
@@ -166,15 +165,33 @@ class Profiler:
         df.dropna(axis=1, how='all', inplace=True)
 
         # Boolean for IsContractAddress, indicated by the icon on BSCscan
-        df["contract_address"] = False
-        df.loc[contract_rows, "contract_address"] = True
+        df["is_contract_address"] = False
+        df.loc[contract_rows, "is_contract_address"] = True
 
-        dead_address = "0x000000000000000000000000000000000000dead"
+        #Stick on the number of holders lol
+        df["num_lp_holders"] = num_lp_holders
 
         return df
 
+    def profile_token(self, address):
+        #Start by querying Poocoin to get BSCScan LP links
+        poocoin_stats = self.query_poocoin(address)
+        
+        #Query token on BSCScan
+        bscscan_stats = self.query_bscscan_token(address)        
+
+        # [Rank, Address, Quantity, Percentage, is_contract_address]
+        v1_lp_holders = self.query_bscscan_liquidity_providers(poocoin_stats["v1_lp_address"])
+        v2_lp_holders = self.query_bscscan_liquidity_providers(poocoin_stats["v2_lp_address"])
+
+        # Check if liquidity is sufficient + locked
+        dead_address = "0x000000000000000000000000000000000000dead"
+
+
 if __name__ == "__main__":
     with Profiler() as profiler:
-        # dd = profiler.query_poocoin("0x2f801292924433f41e34669b0817f990764ecaa0")
-        
-        df = profiler.query_bscscan_liquidity_providers("https://bscscan.com/token/0xef2ec90e0b8d4cdfdb090989ea1bc663f0d680bf#balances")
+        address = "0x8c762ff4721326abc4771b784b8d54bca2af9ad3"
+        profiler.profile_token(address)
+
+
+
